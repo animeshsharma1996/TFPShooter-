e3da04696fcbbb567066d91847285179c0c60fd7
Animesh Sharma <animeshsharma1996@gmail.com>
2021-06-01 04:13:54 +0100

    Saving and loading actor from one map to another

--- TFPShooter/Source/TFPShooter/TFPShooterGameMode.cpp
+++ TFPShooter/Source/TFPShooter/TFPShooterGameMode.cpp
@@ -8,7 +8,6 @@ ATFPShooterGameMode::ATFPShooterGameMode()
 {
 	// set default pawn class to our Blueprinted character
 	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/StylizedModularChar/Blueprints/BP_StylizedModularCharacter"));
-	DefaultPawnClass = NULL;
 
 	/*if (PlayerPawnBPClass.Class != NULL)
 	{
